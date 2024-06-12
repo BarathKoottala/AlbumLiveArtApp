4f2aadcb1603342cffacf4065c80c157b7bd039c
Barath Koottala <barath@Baraths-MBP.lan>
2024-06-11 23:57:30 -0400

    progress updates

--- artwork.py
+++ artwork.py
@@ -7,11 +7,6 @@ from io import BytesIO
 from auth import get_auth_token
 
 def get_artwork(album_id):
-    # sp = spotipy.Spotify(auth=token)
-    # album = "Her Loss"
-    # results = sp.search(id= q = "album:" + album, type = "album")
-    # album_id = results['albums']['items'][0]['uri']
-    # print(results)
     token = get_auth_token()
     parameters = {
         "Content-Type": "application/json",
@@ -22,9 +17,7 @@ def get_artwork(album_id):
     album_url = "https://api.spotify.com/v1/albums/" + album_id
     response = requests.get(album_url, params=parameters)
     print(response.json)
-    img = Image.open(BytesIO(response.content))
     return response
-    # img = Image.open(BytesIO(response.content))
 
 #TODO: Attempt to make function that makes the album artwork resemble a record spinning
 def make_artwork_spin():
--- basic.py
+++ basic.py
@@ -1,9 +1,4 @@
 #!/usr/local/bin/python3
-# import plotly.express as px
-# import plotly.io as pio
-# pio.renderers.default = 'svg'
-# from skimage import io as imgio
-# from skimage.transform import resize
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
@@ -11,13 +6,15 @@ import requests
 from auth import *
 import spotipy.util as util
 from artwork import get_artwork
-import pyfiglet 
+import logging
+
+logger = logging.getLogger('spotify_artwork_app')
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 current_track_id = None
 spotify_cli_client_id = os.environ.get('SPOTIFY_CLI_CLIENT_ID')
 spotify_cli_client_secret = os.environ.get('SPOTIFY_CLI_CLIENT_SECRET')
 redirect_uri = os.environ.get('SPOTIFY_REDIRECT_URI')
-
 
 CURRENT_TRACK_URL = "https://api.spotify.com/v1/me/player"
 
@@ -29,47 +26,50 @@ def get_track():
     
     username = "barath"
     scope = "user-read-currently-playing"
-    print(redirect_uri)
+    global logger
+    logger.info(redirect_uri)
     token = util.prompt_for_user_token(username, scope, spotify_cli_client_id, spotify_cli_client_secret, redirect_uri)
     if token:
         sp = spotipy.Spotify(auth=token)
         results = sp.current_user_playing_track()
         if results == None:
-            print("No track currently playing")
+            logger.info("No track currently playing")
             time.sleep(60)
             return
         item = results["item"]
-        breakpoint()
         album = item["album"]
         album_name = album["name"]
-        track_name = results["item"]["name"]
+        track_name = item["name"]
         artists = album["artists"]
         track_id = item["id"]
         global current_track_id
         if current_track_id == None or current_track_id != track_id:
             current_track_id = track_id
         elif current_track_id == track_id:
-            print("Same song, going to request later")
-            time.sleep(30)
+            logger.info("Same song, going to request later")
+            time.sleep(10)
             return
         artists_name = ', '.join(artist['name'] for artist in artists)
-        sys.stdout.write(f'{colors.GREEN}Currently Playing: {track_name} by {artists_name}\n')
+        logger.info(f'{colors.GREEN}Currently Playing: {track_name} by {artists_name}\n')
     else:
-        print("Can't get token for", username)
+        logger.info("Can't get token for", username)
     
-    print("Making album image request")
+    logger.info("Making album image request")
     album_art_url = album["images"][0]["url"]
     img_data = requests.get(album_art_url).content
-    with open('album_art.jpg', 'wb') as handler:
+    with open('./static/album_art.jpg', 'wb') as handler:
         handler.write(img_data)
         handler.close()
     time.sleep(1)
     
 
 def main():
+    logger.info("Starting")
     result = pyfiglet.figlet_format("Spotify Artwork App") 
-    sys.stdout.write(f'{colors.GREEN}{result}')
+    logger.info(f'{colors.GREEN}{result}')
     while(True):
         get_track()
 
-main()
+if __name__ == "__main__":
+    logger.info("Starting")
+    
